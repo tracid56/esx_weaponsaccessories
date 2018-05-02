c2a2285444cfa02fc6dd5b0a42cf759982194034
Tracid <tracid56@gmail.com>
2018-05-02 13:57:42 +0200

    Update items.sql

--- items.sql
+++ items.sql
@@ -1,22 +1,18 @@
-USE `essentialmode`;
-
-INSERT INTO `items` (id, name, label, limit, rare, can_remove) VALUES
-  (NULL, 'silent','Silencieux',-1, 0, 1),
-  (NULL, 'flashlight','Lampe Torche',-1, 0, 1),
-  (NULL, 'grip','Poignée',-1, 0, 1),
-  (NULL, 'nightvision_scope','Lunette à Vision Nocturne',-1, 0, 1),
-  (NULL, 'thermal_scope','Lunette à Vision Thermique',-1, 0, 1),
-  (NULL, 'extended_magazine','Chargeur Grande Capacitée',-1, 0, 1),
-  (NULL, 'very_extended_magazine','Chargeur Très Grande Capacitée',-1, 0, 1),
-  (NULL, 'scope','Viseur',-1, 0, 1),
-  (NULL, 'advanced_scope','Viseur Avancé',-1, 0, 1),
-  (NULL, 'yusuf','Skin de Luxe',-1, 0, 1),
-  (NULL, 'lowrider','Skin Lowrider',-1, 0, 1),
-  (NULL, 'incendiary','Balles Incendiaires',-1, 0, 1),
-  (NULL, 'tracer_clip','Balles Traceuses',-1, 0, 1),
-  (NULL, 'hollow','Balles à Pointes Creuses',-1, 0, 1),
-  (NULL, 'fmj','Balles Perforantes',-1, 0, 1),
-  (NULL, 'lazer_scope','Viseur Laser',-1, 0, 1),
-  (NULL, 'compansator','Compensateur',-1, 0, 1),
-  (NULL, 'barrel','Canon',-1, 0, 1),
-;
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('silent','Silencieux',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('flashlight','Lampe Torche',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('grip','Poignée',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('nightvision_scope','Lunette à Vision Nocturne',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('thermal_scope','Lunette à Vision Thermique',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('extended_magazine','Chargeur Grande Capacitée',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('very_extended_magazine','Chargeur Très Grande Capacitée',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('scope','Viseur',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('advanced_scope','Viseur Avancé',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('yusuf','Skin de Luxe',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('lowrider','Skin Lowrider',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('incendiary','Balles Incendiaires',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('tracer_clip','Balles Traceuses',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('hollow','Balles à Pointes Creuses',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('fmj','Balles Perforantes',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ( 'lazer_scope','Viseur Laser',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('compansator','Compensateur',-1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('barrel','Canon',-1, 0, 1);
